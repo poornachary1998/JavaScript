de76e2ce980f3eb82cd8987cc94ff9aaebbc2fa5
poorna.narsingosu <poorna.narsingosu@sonesta.com>
2025-08-17 11:07:01 +0530

    Two-sum-practice

--- 08_Interview Preparation: Practice Problems and Coding Questions /01_Arrays DSA/01_Two_Sum.js
+++ 08_Interview Preparation: Practice Problems and Coding Questions /01_Arrays DSA/01_Two_Sum.js
@@ -19,16 +19,14 @@
 // Output: [0,1]
  
 function twoSum(arr, target){
-    let map = new Map();
+let map = new Map();
+for(let i = 0; i< arr.length; i++){
+let complement = target - arr[i];
 
-    for(let i = 0; i< arr.length; i++){
-        let complement = target - arr[i];
-
-        if(map.has(complement)){    
-            return [map.get(complement), i];
-        }
-        map.set(arr[i], i);
-    }
-    
+if(map.has(complement)){
+return [map.get(complement), i]
+} else {
+map.set(arr[i],i)}
 }
-console.log(twoSum([2,7,11,15], 9));
+}
+console.log(twoSum([2,7,11,15], 9))
