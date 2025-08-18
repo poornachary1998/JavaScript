aef89ce5bd85b3983b327baa050a3b77588f30e4
poorna.narsingosu <poornachary1@gmail.com>
2025-08-18 21:57:58 +0530

    Maximum subarray and sum

--- 08_Interview Preparation: Practice Problems and Coding Questions /01_Arrays DSA/04_Maximum_subArray.js
+++ 08_Interview Preparation: Practice Problems and Coding Questions /01_Arrays DSA/04_Maximum_subArray.js
@@ -18,17 +18,41 @@
 // Output: 23
 // Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
  
+// function MaximumSubArray(nums){
+// let maxValue = -Infinity;
+// for(let i = 0; i< nums.length; i++){
+//     let currentSum = 0
+//     for(let j = i; j< nums.length; j++){
+//         currentSum = currentSum + nums[j];
+//         if(currentSum > maxValue){
+//             maxValue = currentSum;
+//         }
+//     }
+// }
+// return maxValue;
+// }
+// console.log(MaximumSubArray([-2,1,-3,4,-1,2,1,-5,4]));
+
+// What if interviewer asks provide me an array of max sum?
+
+ 
 function MaximumSubArray(nums){
 let maxValue = -Infinity;
+let startIdx = 0;
+let endIdx = 0;
 for(let i = 0; i< nums.length; i++){
     let currentSum = 0
     for(let j = i; j< nums.length; j++){
         currentSum = currentSum + nums[j];
         if(currentSum > maxValue){
             maxValue = currentSum;
+            startIdx = i;
+            endIdx = j;
         }
     }
 }
-return maxValue;
+return {maxValue,
+    subArray: nums.slice(startIdx, endIdx + 1)
+};
 }
 console.log(MaximumSubArray([-2,1,-3,4,-1,2,1,-5,4]));
