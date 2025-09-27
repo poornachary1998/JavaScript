727baa0952eb0e1c4b5e62878e701f6a47cbefb6
poorna.narsingosu <poornachary1@gmail.com>
2025-09-27 21:22:35 +0530

    Type Conversion

---
+++ Javascript_Practice/Beginner_Concepts/05_Type_Conversions.js
@@ -0,0 +1,19 @@
+// Type Conversion: When you manually convert a value from one type to another type is called Type conversion.
+
+//To String:
+console.log(String(123)); //123
+
+(123).toString(); //123
+
+//To Number:
+Number("122"); //122
+Number("abc"); //NaN
+parseInt("14px"); //14
+parseFloat("3.14") //3.14
+
+//To Boolean:
+Boolean(1); // true
+Boolean(0); //false
+Boolean(""); //false
+Boolean("hello") // true;
+
