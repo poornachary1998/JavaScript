4dabd6f1eebeddaa62a299c74c01f47373a0cb70
poorna.narsingosu <poornachary1@gmail.com>
2025-10-08 23:27:15 +0530

    this keyword scenario notes

---
+++ Javascript_Practice/Beginner_Concepts/13_this_Keyword.js
@@ -0,0 +1,13 @@
+// this keyword.
+// We will be covering this keyword in many scenarios.
+// 1. "this" in global scope
+// 2. "this" inside a function.
+// 3. "this" inside a "non - strict mode" (this Substitution)
+// 4. "this" value depends on how "this" keyword is called.
+// 5. "this" inside a object's Method.
+// 6. call, apply, bind method ( sharing methods).
+// 7. "this" inside a arrow function.
+// 8. "this" inside a nested arrow function.
+// 9. "this" inside DOM.
+
+
